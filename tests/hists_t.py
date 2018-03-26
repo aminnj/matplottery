87366778002fd9acd419840e788d2afd90040760
Nick Amin <amin.nj@gmail.com>
2018-03-25 18:23:13 -0700

    add some unit tests

---
+++ tests/hists_t.py
@@ -0,0 +1,74 @@
+import unittest
+
+import ROOT as r
+import numpy as np
+from utils import Hist1D, Hist2D, fill_fast
+
+class HistTest(unittest.TestCase):
+
+    def test_1d(self):
+
+        bins = 1.0*np.array([0,3,6,9,12,15])
+        vals = 1.0*np.array([1,2,3,4,5,10,13])
+        hr_ = r.TH1F("hr","hr", len(bins)-1, bins)
+        fill_fast(hr_, vals)
+        hr = Hist1D(hr_)
+        hn = Hist1D(vals,bins=bins)
+
+        self.assertEqual(hn, hr)
+
+        self.assertEqual(hn.get_integral(), np.sum(vals <= np.max(bins)))
+        self.assertEqual(hr.get_integral(), np.sum(vals <= np.max(bins)))
+
+        self.assertEqual(np.all(hn.get_edges() == bins), True)
+        self.assertEqual(np.all(hr.get_edges() == bins), True)
+
+        check = np.histogram(vals,bins=bins)[0]
+        self.assertEqual(np.all(hn.get_counts() == check), True)
+        self.assertEqual(np.all(hr.get_counts() == check), True)
+
+        self.assertEqual(Hist1D(hr_*2), hn*2)
+        self.assertEqual(Hist1D(hr_+hr_), hn+hn)
+
+        self.assertEqual(Hist1D(hr_+0.5*hr_), hn+0.5*hn)
+
+    def test_2d(self):
+        vals2d = 1.0*np.array([
+                [1,1],
+                [1,3],
+                [1,4],
+                [1,4],
+                [3,1],
+                [3,4],
+                ])
+        bins = [
+                np.linspace(0.,4.,3),  # edges 0.0,2.0,4.0
+                np.linspace(0.,5.,3),  # edges 0.0,2.5,5.0
+                ]
+
+        hr_ = r.TH2F("hr2d","hr2d", len(bins[0])-1, bins[0], len(bins[1])-1, bins[1])
+        fill_fast(hr_, vals2d[:,0], vals2d[:,1])
+        hr = Hist2D(hr_)
+
+        hn = Hist2D(vals2d,bins=bins)
+
+        self.assertEqual(hn, hr)
+
+        self.assertEqual(hn.get_integral(), hr.get_integral())
+
+        self.assertEqual(np.all(hr.get_edges()[0] == bins[0]), True)
+        self.assertEqual(np.all(hr.get_edges()[1] == bins[1]), True)
+        self.assertEqual(np.all(hn.get_edges()[0] == bins[0]), True)
+        self.assertEqual(np.all(hn.get_edges()[1] == bins[1]), True)
+
+        hr2x_ = hr_.Clone("hr2x")
+        hr2x_.Scale(2.0)
+        self.assertEqual(Hist2D(hr2x_), hn*2)
+
+        hr2p_ = hr_.Clone("hr2p")
+        hr2p_.Add(hr_)
+        self.assertEqual(Hist2D(hr2p_), hn+hn)
+
+
+if __name__ == "__main__":
+    unittest.main()
