bf29270d1845815028a25b2e52f2fa2489e1a8d0
Nick Amin <amin.nj@gmail.com>
2019-02-19 13:05:38 -0800

    bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='matplottery',
-    version='1.0.4',
+    version='1.0.5',
     description='Nicer histograms with numpy and plotting with matplotlib',
     long_description=long_description,
     long_description_content_type="text/markdown",
