dc5a534b31bcd4e8f37d607ebef2b2e63ae31faf
Nick Amin <amin.nj@gmail.com>
2019-06-02 16:37:28 -0500

    better 0 point for custom palettes

--- matplottery/plotter.py
+++ matplottery/plotter.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 
 import os
 import matplotlib
-matplotlib.use('Agg')
+# matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import warnings
